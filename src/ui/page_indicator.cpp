c41c512931b7a73637d19e5196d78b7cb133b165
Damien Bafile <bafile.damien@gmail.com>
2026-07-03 21:20:45 +0800

    fixed warning

--- src/ui/page_indicator.cpp
+++ src/ui/page_indicator.cpp
@@ -4,7 +4,7 @@
 namespace ui {
 
 void PageIndicator::draw(Framebuffer& fb, int page, int count) const {
-    char buf[16];
+    char buf[32];
     snprintf(buf, sizeof(buf), "< PG %d/%d >", page + 1, count);
     int tw = Framebuffer::textWidth(buf);
     fb.drawText(x_ + (w_ - tw) / 2, y_, buf, fg_, bg_);
